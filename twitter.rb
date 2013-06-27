e96068dc745783e526c7ba9075d420d7e85d66c7
Eric Levine <djlevine@gmail.com>
2013-06-27 17:10:46 -0500

    Added followers identification and light data validation

--- twitter.rb
+++ twitter.rb
@@ -1,34 +1,42 @@
 require 'twitter'
 
 Twitter.configure do |config|
-  config.consumer_key = ""
-  config.consumer_secret = ""
-  config.oauth_token = ""
-  config.oauth_token_secret = ""
+	config.consumer_key = ""
+	config.consumer_secret = ""
+	config.oauth_token = ""
+	config.oauth_token_secret = ""
 end
 
-# Array for handles 
+# Array for Handles 
 tweeter_array_1 = []
+
+# Array for Locations
+@MasterBlaster = {}
 
 # Use search to find handles
 Twitter.search("#Chromatics", :lang => "en", :count => 10).results.each do |tweet|
    tweeter_array_1.push(tweet.from_user) 
 end
 
-# # Find Twitter followers of an artist
-Twitter.followers()
-
-# # Find Twitter followers of an artist
-Twitter.favoriters()
-
-# # Find Twitter followers of an artist
-Twitter.retweeters()
-
-# # Find Twitter followers of an artist
-Twitter.friendships()
+# Find Twitter followers of an artist
+Twitter.followers("mrshaasha").each do |person|
+	if @MasterBlaster.has_key?(person.screen_name)
+  		
+  	else 
+  		@MasterBlaster[screen_name] = person.location
+  	end 
+end
 
 # Find location based on handle
 tweeter_array_1.each do |handle|
-  puts Twitter.user(handle).location
+	if @MasterBlaster.has_key?(handle)
+  		
+  	else 
+  		@MasterBlaster[handle] = Twitter.user(handle).location.downcase!
+  	end
 end
+
+puts @MasterBlaster
+
+
 
