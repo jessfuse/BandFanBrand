17b6764a6f02db7787aff4cb45bd8a8ffe68f957
MakerSquare Student <students@themakersquare.com>
2013-07-10 18:24:29 -0500

    Added pry for results parsing

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source 'https://rubygems.org'
+
+gem 'twitter'
+gem 'json'
+gem 'pry'
+
+http://www.ruby-doc.org/stdlib-2.0/libdoc/uri/rdoc/URI/Escape.html
--- twitter.rb
+++ twitter.rb
@@ -1,12 +1,13 @@
 require 'twitter'
 require 'json'
 require 'open-uri'
+require 'pry'
 
 Twitter.configure do |config|
-	config.consumer_key = ""
-	config.consumer_secret = ""
-	config.oauth_token = ""
-	config.oauth_token_secret = ""
+	config.consumer_key = "WGKsIyF1bIgyMzyOyfPlUw"
+	config.consumer_secret = "opQAAE16rGBMhSN9RhHR2w3rbnLPku4G83kaKChyyI"
+	config.oauth_token = "499825106-3OUNfxYMxxhXXlfVq4Nqx5aeSjwwhainYIbDkR5i"
+	config.oauth_token_secret = "jlDv0dzbckoV1YT8LpqlvjAqozU6lFmXYKLOYDW8"
 end
 
 # Array for Handles 
@@ -24,14 +25,15 @@ tweeter_array_1 = []
 #User Input Dialogue
 puts "What band or album do you want to look for?"
 @search_term = "#" + gets.chomp!
-puts "Cool. What is the Twitter handle of the band?"
-@search_handle = gets.chomp!
 puts "Cool.  Wait a second for the results."
+# puts "What is the Twitter handle of the band?"
+# @search_handle = gets.chomp!
+# puts "Cool.  Wait a second for the results."
 # puts @search_term
 # puts @search_handle
 
 # Use search to find handles
-Twitter.search("#{@search_term}", :lang => "en", :count => 10).results.each do |tweet|
+Twitter.search("#{@search_term}", :lang => "en", :count => 20).results.each do |tweet|
    tweeter_array_1.push(tweet.from_user) 
 end
 
@@ -46,21 +48,27 @@ def google_locater(location)
   end
   uri = "https://maps.googleapis.com/maps/api/geocode/json?address=#{@usable_address.gsub(' ', '%20').gsub('ÜT:', '')}&sensor=false"
   response = open(uri).read
-  final_address = JSON.parse(response)["results"][0]["formatted_address"]
-  @markers.push(final_address)
-  return final_address
+  parsed_response = JSON.parse(response)
+  if parsed_response["results"].empty?
+    return nil
+  else
+    final_address = parsed_response["results"][0]["formatted_address"]
+    @markers.push(final_address)
+    return final_address
+  end
 end
 
+
 # Find Twitter followers of an artist
-Twitter.followers("#{@search_handle}").each do |guy|
-  location_entry = google_locater(guy.location)
-  @MasterBlaster << {:user_name => guy.screen_name, :location => location_entry}
-end
+# Twitter.followers("#{@search_handle}").each do |guy|
+#   location_entry = google_locater(guy.location)
+#   @MasterBlaster << {:user_name => guy.screen_name, :location => location_entry}
+# end
 
 # Find location based on handle
 tweeter_array_1.each do |handle|
   location_entry = google_locater("#{Twitter.user(handle).location}")
-  @MasterBlaster << { :user_name => handle, :location => location_entry }
+  @MasterBlaster << { :user_name => handle, :location => location_entry } if location_entry
 end
 
 
